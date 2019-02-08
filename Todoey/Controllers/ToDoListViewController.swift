4890544112986aa03ea59b4d27475b0c3f0254d9
Morgan Hondros <morganhondros@gmail.com>
2019-02-08 12:37:50 -0500

    Done with Delete part of crud. Confused about just adding a checkmark and the item doesn't go away. Question posted on Udemy.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -63,13 +63,18 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-//        context.delete(todoItems[indexPath.row])
-//        todoItems?.remove(at: indexPath.row)
+        if let item = todoItems?[indexPath.row] {
+            do {
+            try realm.write {
+//                realm.delete(item)
+                item.done = !item.done
+            }
+            } catch {
+                print("ERROR UPDATING ITEMS: \(error)")
+            }
+        }
         
-        
-//        todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-//
-//        saveItems()
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
